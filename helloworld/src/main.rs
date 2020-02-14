94e6f984458dd862c0f179f3a40a0442cc71b723
lpxxn <mi_duo@live.com>
2020-02-14 12:56:30 +0800

    hello world

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,3 +1,12 @@
+const ABC: u32 = 123445;
 fn main() {
+    let a = 1;
+    println!("a = {}", a);
+    
+    let mut b: u32 = 1;
+    println!("b = {}", b);
+    b = 3;
+    println!("b = {}", b);
+    println!("const value = {}", ABC);
     println!("Hello, world!");
 }
