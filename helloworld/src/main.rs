2c13c0012eebbfd559d9f15a5929038dffe7a72b
lpxxn <mi_duo@live.com>
2020-02-14 13:52:34 +0800

    hello world

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -9,4 +9,91 @@ fn main() {
     println!("b = {}", b);
     println!("const value = {}", ABC);
     println!("Hello, world!");
+    // data type
+    // bool 
+    let verify: bool = true;
+    println!("verify: {}", verify);
+
+    // char rust里 char是32位的也就是说一个car可以是一个汉字，c++里是8位
+    let a = 'a';
+    println!("a = {}", a);
+    let b: char = '中';
+    println!("b = {}", b);
+   
+    
+    // i8, i16, i32, i64, u8, u16, u32, u64, f32, f64
+    let i8v: i8 = -123;
+    println!("i8v = {}", i8v);
+
+    let f32v: f32 = 0.00013;
+    println!("f32v = {}", f32v);
+
+    // 自适应 isize, usize
+    println!("max = {}", usize::max_value());
+    
+    // 数组 [type; size]
+    let arr: [u32; 5] = [1, 2 ,3 ,4, 5];
+    println!("arr = {:?}", arr);
+    let arr1: [u32; 3] = [1, 2, 3];
+    show(arr1);
+    println!("end array");
+
+    // 元组
+    let tup1: (i32, f64, char) = (123, 0.23, '元');
+    println!("t1= {}, t2= {}, t3= {}", tup1.0, tup1.1, tup1.2);
+    let (x, y, z) = tup1;
+    println!("x= {}, y= {}, z= {}", x, y, z);
+    
+    println!("add = {}", add(1, 2));
+
+    // 表达式 可以有返回值
+    let y = {
+        let x = 1;
+        // 也是没有;
+        x + 2
+    };
+    println!("y = {}", y);
+
+    //控制流
+    let control = true;
+    let x = if control {
+        5
+    } else {
+        8
+    };
+    println!("x = {}", x);
+    // loop
+    let mut  control = 0;
+    loop {
+       println!("in loop");
+       control +=  1;
+       if control == 10 {
+          break;
+       }
+    }
+
+    control = 0;
+
+    let x = loop {
+       println!("in loop");
+       control +=  1;
+       if control == 10 {
+          break control * 5;
+       }
+    };
+    println!("x = {}", x);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+   // 可以这么写 注意最后没有;
+   // let result = a + b;
+   // result
+   // 也可以这么写
+   a + b
+}
+
+fn show(arr: [u32; 3]) {
+    for i in &arr {
+        println!("{}", i)
+    }
 }
