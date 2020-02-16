15efd146b807112fe65d0c8423556f1d9c5de7f2
lpxxn <mi_duo@live.com>
2020-02-16 17:53:30 +0800

    struct

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -15,4 +15,12 @@ fn main() {
     let a = [1, 2, 3, 4];
     let as1 = &a[0..3];
     println!("as1 = {:?}", as1);
+
+    // slice 必需是utf-8的字符串，如果是中文会报错
+    /*
+    let z = String::from("中文字符");
+    //'byte index 1 is not a char boundary; it is inside '中' (bytes 0..3) of `中文字符`
+    let zs1 = &z[0..1];
+    */
+    
 }
