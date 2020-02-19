3ecce565b7e8154b553e909a7a3a9361f599867f
lpxxn <mi_duo@live.com>
2020-02-19 12:32:45 +0800

    trait

--- z_2_trait/src/main.rs
+++ z_2_trait/src/main.rs
@@ -5,6 +5,14 @@ pub trait GetInformation {
     fn get_age(&self) -> i32;
 }
 
+trait School {
+    fn get_school_name(&self) -> String {
+        //String::from("小星星学校")
+        "小星星学校".to_string()
+    }
+}
+
+
 #[derive(Debug)]
 pub struct Student {
     pub name : String,
@@ -18,6 +26,12 @@ impl GetInformation for Student {
 
     fn get_age(&self) -> i32 {
         self.age
+    }
+}
+
+impl School for Student {
+    fn get_school_name(&self) -> String {
+        "abcde school".to_string()
     }
 }
 
@@ -36,19 +50,38 @@ impl GetInformation for Teacher {
         self.age
     }
 }
+impl School for Teacher {
+    
+}
 
-fn print_information(item: impl GetInformation) {
+fn print_information(item: &impl GetInformation) {
     println!("items name: {}, age: {}", item.get_name(), item.get_age());
- 
+}
+
+fn print_information2<T: GetInformation>(item: &T) {
+    println!("T name: {}, age: {}", item.get_name(), item.get_age());
+}
+
+fn print_information3<T: GetInformation + School>(item: &T) {
+    println!("T3fn school: {}, name: {}, age: {}", item.get_school_name(), item.get_name(), item.get_age());
+}
+
+fn print_school_information(item: impl School) {
+    println!("item school name: {}", item.get_school_name());
 }
 
 
 fn main() {
     let s = Student{name: "xiao".to_string(), age: 10};
     println!("s name: {}, age: {}", s.get_name(), s.get_age());
-    print_information(s);
+    print_information(&s);
+    println!("school {}", s.get_school_name());
+    print_information3(&s);
+    print_school_information(s);
     let t = Teacher{name: "da".to_string(), age: 20};
-    print_information(t);
+    print_information2(&t);
+    print_school_information(t);
+
 
     println!("Hello, world!");
 }
