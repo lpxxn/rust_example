71b13ed968b252281efda446d8d3b5b9abf2d600
lpxxn <mi_duo@live.com>
2020-02-19 11:49:45 +0800

    generic

--- z_2_trait/src/main.rs
+++ z_2_trait/src/main.rs
@@ -1,3 +1,28 @@
+// trait 抽象方式定义共享行为 类似于其他语言的interface
+
+pub trait GetInformation{
+    fn get_name() -> &String;
+    fn get_age() -> u32;
+}
+
+#[derive(Debug)]
+pub struct Student {
+    pub name : String,
+    age: i32,
+}
+
+impl GetInformation for Student {
+    fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    fn get_age(&self) -> u32 {
+        &self.age
+    }
+}
+
 fn main() {
+    let s = Student{name: "xiao", age: 10};
+    println!("s name: {}, age: {}", s.get_name(), s.get_age()());
     println!("Hello, world!");
 }
