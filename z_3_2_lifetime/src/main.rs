b2a6ef44795c81953f78f2dbf1c576081d7b64ba
lpxxn <mi_duo@live.com>
2020-02-20 12:11:59 +0800

    lifetime

---
+++ z_3_2_lifetime/src/main.rs
@@ -0,0 +1,23 @@
+// 静态生命周期 'static
+// 生命周期存活于整个程序期间，所有字符字面值都拥有static生命周期。
+// let s: &'static str = "hello";
+
+use std::fmt::Display;
+fn do_something<'a, T: Display>(x: &'a str, y: &'a str, ann: T) -> &'a str {
+    println!("ann is {}", ann);
+    if x.len() < y.len() {
+        x
+    } else {
+        y
+    }
+}
+fn main() {
+    let s1 = String::from("s1");
+    let s2 = String::from("s2");
+    let ann = 123;
+    let r = do_something(s1.as_str(), s2.as_str(), ann);
+    println!("r {}", r);
+    
+
+    println!("Hello, world!");
+}
