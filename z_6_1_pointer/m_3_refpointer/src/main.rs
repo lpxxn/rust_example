e659a333ce648bd2447d25dcf34c3166785d8e5f
lpxxn <mi_duo@live.com>
2020-02-21 17:08:19 +0800

    rc

--- z_6_1_pointer/m_3_refpointer/src/main.rs
+++ z_6_1_pointer/m_3_refpointer/src/main.rs
@@ -11,7 +11,7 @@ enum List2 {
 //use self::List::{Cons, Nil};
 use crate::List::{Cons, Nil};
 use std::rc::Rc;
-
+// Rc<T> 允许程序的多个部分之间只读的共享数据。
 
 fn main() {
     let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
@@ -20,9 +20,16 @@ fn main() {
     // let c = Cons(4, Box::new(a));
 
     let r1 = Rc::new(List2::Cons(1, Rc::new(List2::Cons(10, Rc::new(List2::Nil)))));
+    println!("count afeter creating r1 {}", Rc::strong_count(&r1));
     let r2 = List2::Cons(2, Rc::clone(&r1));
-    let r3 = List2::Cons(3, r1.clone());
-    let r4 = List2::Cons(4, r1.clone());
+    println!("count afeter creating r1 {}", Rc::strong_count(&r1));
+    {
+        let r3 = List2::Cons(3, r1.clone());
+        println!("count afeter creating r1 {}", Rc::strong_count(&r1));
+        let r4 = List2::Cons(4, r1.clone());
+        println!("count afeter creating r1 {}", Rc::strong_count(&r1));
+    }
+    println!("count afeter creating r1 {}", Rc::strong_count(&r1));
 
     println!("r1 {:?}", r1);
     println!("r1 {:?}", &r1);
