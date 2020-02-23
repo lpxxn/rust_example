72a8508fbaab7384796e8091722f48cd2ef54e04
lpxxn <mi_duo@live.com>
2020-02-23 13:34:55 +0800

    trait

--- z_9_origin_object/o_b_1/aver_collect/src/lib.rs
+++ z_9_origin_object/o_b_1/aver_collect/src/lib.rs
@@ -2,6 +2,20 @@ pub trait Draw: std::fmt::Debug {
     fn draw(&self);
 }
 
+// 使用 trait 对象时，使用的是动态分发，在运行时，通过trait的指针来调用具体的方法。
+// trait 对象要求对象安全
+// 只有对象安全(object safe)的trait才可以组成trait对象。trait的方法满足以下两条要求
+// 才是对象安全的：
+// 1.返回值类型不为 Self eg: Clone trait 下面有错误例子
+// 2.方法没有任何泛型类型参数 
+/*
+#[derive(Debug)]
+struct TestNotSafe {
+    pub c: Clone,
+    // error the trait `std::clone::Clone` cannot be made into an object
+}
+*/
+
 #[derive(Debug)]
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,// trait 对象，使用dyn关键字
@@ -15,7 +29,8 @@ impl Screen {
     }
 }
 
-// trait 单态化处理，是静态分法，编译时就知道了具体类型
+// trait bound 编译器进行的方式是单态化处理，进行的是静态分发，
+//也就是说编译器在编译时就知道调用了什么方法，编译时就知道了具体类型
 // 第一次传入的是谁，以后都要是谁
 // #[derive(Debug)]
 // struct Screen2<T: Draw> {
