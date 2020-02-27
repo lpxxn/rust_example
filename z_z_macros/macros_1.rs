bcff2398b27ab0a94e23014c059085c55e908dd7
lpxxn <mi_duo@live.com>
2020-02-27 16:42:06 +0800

    macro

--- z_z_macros/macros_1.rs
+++ z_z_macros/macros_1.rs
@@ -9,4 +9,16 @@ fn main() {
     hello!{};
 }
 
+/*
+#[derive(Serialize)]
+struct Fool {
+    bar: usize,
+}
+generqte someting like..
+impl Serialize fro Fool {
+    ...
+}
+*/
+// https://github.com/dtolnay/cargo-expand
+
 //rustc --out-dir ./target ./macros_1.rs
