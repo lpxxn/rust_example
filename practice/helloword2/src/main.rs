1238d2c75842ab0b2a1e3a1d5a7de2fc3c5efe4d
lpxxn <mi_duo@live.com>
2022-06-13 21:31:51 +0800

    feat: readme

--- practice/helloword2/src/main.rs
+++ practice/helloword2/src/main.rs
@@ -15,4 +15,21 @@ fn main() {
     println!("{}", r#"你好"你好""#);
     // 如果还有歧义,比如字符串内有相同数量的#，可以继续加 #
     println!("{}", r###"abcd # deffe"##"###);
+    testString();
+}
+
+fn testString() {
+    let mut s = String::from("hello, world");
+
+    let slice1: &str = &s; // 使用两种方法
+    assert_eq!(slice1, "hello, world");
+ 
+    let slice2 = &s[..5];
+    assert_eq!(slice2, "hello");
+ 
+    //Note! The type here cant be `&mut str` due to `push` is ONLY defined on String type and its mut reference: `&mut String` !
+    // So you can't use `s.as_mut_str()`
+    let slice3: &mut String = &mut s;
+    slice3.push('!');
+    assert_eq!(slice3, "hello, world!");
 }
