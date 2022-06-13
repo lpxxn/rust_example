f58a315a108011c13ce2d5a7f76ebe0665152509
lpxxn <mi_duo@live.com>
2022-06-13 10:48:50 +0800

    string

--- practice/helloword/src/main.rs
+++ practice/helloword/src/main.rs
@@ -1,3 +1,21 @@
+
 fn main() {
     println!("Hello, world!");
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    println!("a: {} a bit: {:x}", abc.1, abc.1.to_bits());
+
+    let x = '中';
+    println!("字符'中'占用了{}字节的内存大小",std::mem::size_of_val(&x));
+
+    let x = "你好，世界";
+    for c in x.chars() {
+        println!("{}", c);
+    }
+
+    let x = "holla中国人नमस्ते";
+    for c in x.chars() {
+        println!("{}", c);
+    }
+    utf8_slice::
+    // 用这个库 得到子串信息 https://crates.io/crates/utf8_slice
 }
--- practice/helloword2/src/main.rs
+++ practice/helloword2/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    let s = String::from("hello");
+    let slice = &s[..2];
+    let slice = &s[0..2];
+    let slice = &s[..s.len()];
+    // \ 用来转义
+    println!("{}", "hello \x52\x75\x73\x74");
+    // 不想转义
+    println!("{}", "hello \\x52\\x75\\x73\\x74");
+    // 用r
+    let raw_str = r"hello \x52\x75\x73\x74";
+    println!("{}", raw_str);
+    // 如果有 " 可以加 # 并以## 结束
+    println!("{}", r#"你好"你好""#);
+    // 如果还有歧义,比如字符串内有相同数量的#，可以继续加 #
+    println!("{}", r###"abcd # deffe"##"###);
 }
