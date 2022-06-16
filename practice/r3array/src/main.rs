46157560151d0918e3d4b42ffcd9cefaaf9482ac
lpxxn <mi_duo@live.com>
2022-06-16 13:15:07 +0800

    feat: rust test

--- practice/r3array/src/main.rs
+++ practice/r3array/src/main.rs
@@ -23,3 +23,10 @@ fn main() {
     println!("index: {}", a2[2]);
 
 }
+
+/*
+使用方法	                   等价使用方式	                                        所有权
+for item in collection	     for item in IntoIterator::into_iter(collection)	转移所有权
+for item in &collection	     for item in collection.iter()	                    不可变借用
+for item in &mut collection	 for item in collection.iter_mut()	                可变借用
+*/
