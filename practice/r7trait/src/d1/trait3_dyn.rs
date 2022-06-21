155dfdb146005fbf40c3c6a4faf8373fef0213ba
lpxxn <mi_duo@live.com>
2022-06-21 09:57:34 +0800

    feat: test

--- practice/r7trait/src/d1/trait3_dyn.rs
+++ practice/r7trait/src/d1/trait3_dyn.rs
@@ -25,3 +25,32 @@ fn test1() {
     my_func1(12_u32);
     my_func1("abc".to_string());
 }
+
+
+trait MyTrait2 {
+    fn f(&self) -> Box<dyn MyTrait2>;
+}
+
+impl MyTrait2 for u32 {
+    fn f(&self) -> Box<dyn MyTrait2> {
+        println!("u32");
+        Box::new(1)
+    }
+}
+
+impl MyTrait2 for String {
+    fn f(&self) -> Box<dyn MyTrait2> {
+       println!("string");
+        Box::new(self.clone())
+    }
+}
+
+fn my_func2(x: Box<dyn MyTrait2>) -> Box<dyn MyTrait2> {
+    x.f()
+}
+
+#[test]
+fn test2() {
+    my_func2(Box::new(123));
+    my_func2(Box::new("abc".to_string()));
+}
