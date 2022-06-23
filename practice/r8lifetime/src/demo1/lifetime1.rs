a90b933374bbe93270b8d401e208e937585686df
lpxxn <mi_duo@live.com>
2022-06-23 10:09:31 +0800

    feat: test

--- practice/r8lifetime/src/demo1/lifetime1.rs
+++ practice/r8lifetime/src/demo1/lifetime1.rs
@@ -34,7 +34,7 @@ fn test_lifetime() {
     println!("Interface should be dropped here and the borrow released");
     // 下面的调用会失败，因为同时有不可变/可变借用
     // 但是Interface在之前调用完后就应该被释放
-    use_list(&list);
+    //use_list(&list);
     // 由于get_interface的方法把自己的 manager的借给了Interface，但是interface的生命周期和list是一样长。所以
     // 没有被释放掉。
 }
@@ -42,3 +42,10 @@ fn test_lifetime() {
 fn use_list(list: &List) {
     println!("{}", list.manager.text);
 }
+
+/*
+这是因为我们在 get_interface 方法中声明的 lifetime 有问题，该方法的参数的生命周期是 'a，
+而 List 的生命周期也是 'a，说明该方法至少活得跟 List 一样久，再回到 test 函数中，
+list 可以活到 test 函数的结束，因此 list.get_interface() 借用的可变引用也会活到 test 函数的结束，
+在此期间，自然无法再进行借用了。
+ */
--- practice/r8lifetime/src/demo1/lifetime2.rs
+++ practice/r8lifetime/src/demo1/lifetime2.rs
@@ -11,3 +11,34 @@ impl<'a: 'b, 'b> Interface<'a, 'b> {
 struct Manager<'a> {
     text: &'a str
 }
+
+// 我们需要为 get_interface 方法的参数给予一个不同于 List<'a> 的生命周期 'b
+struct List<'a> {
+    manager: Manager<'a>
+}
+
+impl<'a> List<'a> {
+    // 也可以说是给 Interface 一个小于'a 的生命周期
+    pub fn get_interface<'b>(&'b mut self) -> Interface<'a, 'b>
+        where 'a: 'b {
+        Interface {
+            manager: &mut self.manager
+        }
+    }
+}
+
+#[test]
+fn test_lifetime() {
+    let mut list = List {
+        manager: Manager{
+            text: "hello"
+        }
+    };
+    list.get_interface().noop();
+
+    use_list(&list);
+}
+
+fn use_list(list: &List) {
+    println!("{}", list.manager.text)
+}
