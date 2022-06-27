02b8fbc5c9e316760299ec9ac76c0cc94cccf0fc
lpxxn <mi_duo@live.com>
2022-06-27 22:08:48 +0800

    :feat: box

--- practice/r10box/src/main.rs
+++ practice/r10box/src/main.rs
@@ -4,8 +4,24 @@ fn main() {
     let y = MyBox::new(123);
     println!("v: {}", *y);
     println!("v: {}", *y);
+    // 必须使用 &s 的方式来触发 Deref(仅引用类型的实参才会触发自动解引用)
+    displayMyBox(&y);
+
+    let s = String::from("hello world");
+    display(&s);
+}
+fn displayMyBox(b: &i32) {
+    println!("{}", b);
 }
 
+fn display(s: &str) {
+    println!("{}", s);
+}
+/*
+String 实现了 Deref 特征，可以在需要时自动被转换为 &str 类型
+&s 是一个 &String 类型，当它被传给 display 函数时，自动通过 Deref 转换成了 &str
+必须使用 &s 的方式来触发 Deref(仅引用类型的实参才会触发自动解引用)
+ */
 
 struct MyBox<T>(T);
 
