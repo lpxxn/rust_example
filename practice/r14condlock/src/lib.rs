a0dcee126e0a7ceaff37ee839f72a7a6b0e944e7
lpxxn <mi_duo@live.com>
2022-07-03 21:49:04 +0800

    feat: arc mutex

--- practice/r14condlock/src/lib.rs
+++ practice/r14condlock/src/lib.rs
@@ -1,5 +1,22 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 #[test]
 fn it_works() {
     // 通过 Rc实现Mutex的多所有权
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
