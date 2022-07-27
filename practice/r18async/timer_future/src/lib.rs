91b162252b815883632cfeb10bf3d9df3427bb29
lpxxn <mi_duo@live.com>
2022-07-27 13:28:03 +0800

    feat: sync

--- practice/r18async/timer_future/src/lib.rs
+++ practice/r18async/timer_future/src/lib.rs
@@ -1,3 +1,24 @@
+use std::{future::Future, pin::Pin,
+        sync::{Arc, Mutex},
+        task::{Context, Poll, Wake},
+        thread,
+        time::Duration,
+};
+use std::task::Waker;
+
+/// 在Future 和等待的线程间共享状态
+struct SharedState {
+    /// 定制（睡眠）是否结束
+    completed: bool,
+    /// 当睡眠结束后，线程可以用`waker`通知`TimerFuture`来唤醒任务
+    waker: Option<Waker>
+}
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+
 
 
 #[test]
